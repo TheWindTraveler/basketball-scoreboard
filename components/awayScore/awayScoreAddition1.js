46772d12d202f1edd8e814710af9d4d06835b327
Begotten-Traveler <solomondev002@gmail.com>
2025-03-23 08:22:15 +0100

    add: awayScoreAddition2 component

--- components/awayScore/awayScoreAddition1.js
+++ components/awayScore/awayScoreAddition1.js
@@ -2,7 +2,7 @@
 let awayAdd1 = document.getElementsByClassName("away-add-1");
 
 
-// Awat score (1) function //
+// Away score (1) function //
 function awayScoreAddition1(){
    awayScoreCount = awayScoreCount + 1;
    awayScore.innerText = awayScoreCount;
---
+++ components/awayScore/awayScoreAddition2.js
@@ -0,0 +1,8 @@
+// Away score (2) variable //
+let awayAdd2 = document.getElementsByClassName("away-add-2");
+
+// Away score (2) function //
+function awayScoreAddition2(){
+    awayScoreCount = awayScoreCount + 2;
+    awayScore.innerText = awayScoreCount;
+}
