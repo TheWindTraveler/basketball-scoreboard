157408c01cec3f8ef45fda01302220394e778ddb
Begotten-Traveler <solomondev002@gmail.com>
2025-03-23 08:15:23 +0100

    add: awayScoreAddition1 component

---
+++ components/awayScore/awayScoreAddition1.js
@@ -0,0 +1,9 @@
+// Away score (1) variable //
+let awayAdd1 = document.getElementsByClassName("away-add-1");
+
+
+// Awat score (1) function //
+function awayScoreAddition1(){
+   awayScoreCount = awayScoreCount + 1;
+   awayScore.innerText = awayScoreCount;
+}
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Reusable home score variable //
 let homeScore = document.getElementById("home-score");
+let awayScore = document.getElementById("away-score");
 
 // Reusable variable
 let homeScoreCount = 0;
+let awayScoreCount = 0;
