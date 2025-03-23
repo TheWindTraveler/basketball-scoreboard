39f412aa859c34999d9378f6f087341d70798d99
Begotten-Traveler <solomondev002@gmail.com>
2025-03-23 07:07:16 +0100

    add: JavaScript variables declaration with the DOM

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("JS properly linked")
+// Home variables
+let homeScore = document.getElementById("home-score");
+let homeAdd1 = document.getElementsByClassName("home-add-1");
+let homeAdd2 = document.getElementsByClassName("home-add-2");
+let homeAdd3 = document.getElementsByClassName("home-add-3");
+
+// Away variables
+let awayScore = document.getElementById("away-score");
+let awayAdd1 = document.getElementsByClassName("away-add-1");
+let awayAdd2 = document.getElementsByClassName("away-add-2");
+let awayAdd3 = document.getElementsByClassName("away-add-3");
