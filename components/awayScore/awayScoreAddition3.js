073990d8910e6d3302a9e54fa2547d3d213788d1
Begotten-Traveler <solomondev002@gmail.com>
2025-03-23 08:26:52 +0100

    awayScoreAddition3 component

---
+++ components/awayScore/awayScoreAddition3.js
@@ -0,0 +1,9 @@
+// Away score (3) variable //
+let awayAdd3 = document.getElementsByClassName("away-add-3");
+
+
+// Away score (3) function //
+function awayScoreAddition3(){
+    awayScoreCount = awayScoreCount + 3;
+    awayScore.innerText = awayScoreCount;
+}
